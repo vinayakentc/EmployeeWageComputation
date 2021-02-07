db095549e8873212644b2424310038d719eda9f3
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-08 00:33:22 +0530

    implement_interface_in empwagecomputation

--- src/EmployeeWage/EmployeeWageComputation.java
+++ src/EmployeeWage/EmployeeWageComputation.java
@@ -1,5 +1,12 @@
 package EmployeeWage;
 
+interface IEmployeeWageComputation
+{
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
+
+    public void calculateTotalWage();
+}
+
 class CompanyEmpWage
 {
     // instance constants
@@ -24,6 +31,7 @@ class CompanyEmpWage
         this.totalEmpWage = totalEmpWage;
     }
 
+    @Override
     public String toString()
     {
         System.out.println("Details of " + COMPANY_NAME + " employee");
@@ -35,7 +43,7 @@ class CompanyEmpWage
     }
 }
 
-public class EmployeeWageComputation
+class EmployeeWageComputation implements IEmployeeWageComputation
 {
     // class constants
     public static final int PART_TIME = 1;
@@ -51,7 +59,7 @@ public class EmployeeWageComputation
         index = 0;
     }
 
-    void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
     {
         companies[index++] = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
     }
@@ -65,16 +73,16 @@ public class EmployeeWageComputation
     {
         switch (empType)
         {
-        case FULL_TIME:
-            return 8;
-        case PART_TIME:
-            return 4;
-        default:
-            return 0;
+            case FULL_TIME:
+                return 8;
+            case PART_TIME:
+                return 4;
+            default:
+                return 0;
         }
     }
 
-    void calculateTotalWage()
+    public void calculateTotalWage()
     {
         for (CompanyEmpWage company : companies)
         {
